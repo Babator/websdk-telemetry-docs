5c76cb9a3b347a123bab3773b62363d6eb48d6e7
yevkbb <yevk@babator.com>
2016-11-09 13:12:47 +0200

    Update telemetry-example.js

--- telemetry-example.js
+++ telemetry-example.js
@@ -1,21 +1,9 @@
-if (Babator && Babator.isLoaded) {
+if (window.Babator && window.Babator.isReady) {
   Babator.telemetry.on('start', function (event, data) {
     console.log('[Video Start] Data: ' + JSON.stringify(data, null, 2));
   });
   
-  Babator.telemetry.on('autoStart', function (event, data) {
-    console.log('[Video Auto Start] Data: ' + JSON.stringify(data, null, 2));
-  });
-  
-  Babator.telemetry.on('autoFirst', function (event, data) {
-    console.log('[Video Auto First] Data: ' + JSON.stringify(data, null, 2));
-  });
-  
-  Babator.telemetry.on('recommendationClick', function (event, data) {
-    console.log('[Video Recommendation Clicked] Data: ' + JSON.stringify(data, null, 2));
-  });
-  
-  Babator.telemetry.on('autoPlay', function (event, data) {
-    console.log('[Video Auto Play] Data: ' + JSON.stringify(data, null, 2));
+  Babator.telemetry.on('request', function (event, data) {
+    console.log('[Video Request] Data: ' + JSON.stringify(data, null, 2));
   });
 }
