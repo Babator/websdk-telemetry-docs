3beac8db3c52837f984221cd564d87bd8114b835
yevkbb <yevk@babator.com>
2016-11-09 15:25:47 +0200

    Update telemetry-example.js

--- telemetry-example.js
+++ telemetry-example.js
@@ -1,9 +1,22 @@
-if (window.Babator && window.Babator.isReady) {
-  Babator.telemetry.on('start', function (event, data) {
-    console.log('[Video Start] Data: ' + JSON.stringify(data, null, 2));
+;(function () {
+  function onBabatorReady(callback) {
+    if (window.Babator && window.Babator.isReady) {
+      callback();
+    } else {
+      window.addEventListener('babator-ready', function babatorReadyListener() {
+        window.removeEventListener('babator-ready', babatorReadyListener);
+        callback();
+      });
+    }
+  }
+
+  onBabatorReady(function () {
+    Babator.telemetry.on('start', function (event, data) {
+      console.log('[Video Start] Data: ' + JSON.stringify(data, null, 2));
+    });
+
+    Babator.telemetry.on('request', function (event, data) {
+      console.log('[Video Request] Data: ' + JSON.stringify(data, null, 2));
+    });
   });
-  
-  Babator.telemetry.on('request', function (event, data) {
-    console.log('[Video Request] Data: ' + JSON.stringify(data, null, 2));
-  });
-}
+})();
